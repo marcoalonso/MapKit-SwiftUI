2c39d561b610553663f4c7250a1e8354eb099f07
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2024-02-12 17:58:14 -0600

    Getting started

---
+++ MapsSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapsSwiftUI
+//
+//  Created by Marco Alonso Rodriguez on 12/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapsSwiftUI/MapsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsSwiftUIApp.swift
+//  MapsSwiftUI
+//
+//  Created by Marco Alonso Rodriguez on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MapsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
