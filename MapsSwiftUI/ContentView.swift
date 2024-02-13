77d2f83c8664b8ac38d238ecfd06313a626ea733
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2024-02-12 18:10:26 -0600

    Map of CDMX and showing custom Annotation

--- MapsSwiftUI/ContentView.swift
+++ MapsSwiftUI/ContentView.swift
@@ -6,19 +6,51 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var cameraPosition: MapCameraPosition = .region(.userRegion)
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Map(position: $cameraPosition) {
+            // Marker("My location", coordinate: .userLocation)
+               // .tint(.blue)
+            
+            Annotation("My location", coordinate: .userLocation) {
+                ZStack {
+                    Circle()
+                        .frame(width: 32, height: 32)
+                        .foregroundStyle(.blue.opacity(0.25))
+                    
+                    Circle()
+                        .frame(width: 20, height: 20)
+                        .foregroundStyle(.white)
+                    
+                    Circle()
+                        .frame(width: 12, height: 12)
+                        .foregroundStyle(.blue)
+                }
+            }
         }
-        .padding()
+        .mapControls {
+            MapCompass()
+            MapPitchToggle()
+            MapUserLocationButton()
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+extension CLLocationCoordinate2D {
+    static var userLocation: CLLocationCoordinate2D {
+        return .init(latitude: 19.428345, longitude: -99.18173)
+    }
+}
+
+extension MKCoordinateRegion {
+    static var userRegion: MKCoordinateRegion {
+        return .init(center: .userLocation, latitudinalMeters: 5000, longitudinalMeters: 5000)
+    }
 }
